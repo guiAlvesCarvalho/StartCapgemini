8661441d27f6006a8a1c36ad2d43755f9d9579cb
Guilherme Alves <guialvescarvalho.gac@gmail.com>
2023-05-25 22:02:28 -0300

    TaskController

--- TodoApp/app/src/main/java/util/ConnectionFactory.java
+++ TodoApp/app/src/main/java/util/ConnectionFactory.java
@@ -6,6 +6,8 @@ package util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  *
@@ -22,8 +24,8 @@ public class ConnectionFactory {
         try {
             Class.forName(DRIVER);
             return DriverManager.getConnection(URL, USER, PASS);
-        } catch (Exception ex) {
-            throw new RuntimeException("Erro na conexão com o banco de dados", ex);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro na conexão com o banco de dados", e);
         }
     }
     
@@ -32,8 +34,44 @@ public class ConnectionFactory {
             if (connection != null) {
                 connection.close();
             }
-        } catch (Exception ex) {
-            throw new RuntimeException("Erro ao fechar a conexão com o banco de dados", ex);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao fechar a conexão com o banco de dados", e);
+        }
+    }
+    
+    public static void closeConnection(Connection connection, PreparedStatement statement) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+            
+            if (statement != null) {
+                statement.close();
+            }
+            
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao fechar a conexão com o banco de dados", e);
+        }
+    }
+    
+    public static void closeConnection(Connection connection, 
+            PreparedStatement statement, ResultSet resultSet) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+            
+            if (statement != null) {
+                statement.close();
+            }
+            
+            if (resultSet != null) {
+                resultSet.close();
+                
+            }
+            
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao fechar a conexão com o banco de dados", e);
         }
     }
     
